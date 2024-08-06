096cad077792fa065b83e757db3a77bac829270c
Shafeen <shafeenes@gmail.com>
2024-08-06 05:59:11 +0300

    w

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3747381728"
+    serviceWorkerVersion: "4050185041"
   }
 });
