2206ea2e183b8a37c4b977a56e89cc737f85238e
Shafeen <shafeenes@gmail.com>
2025-03-31 16:13:26 +0300

    e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "26c6b19aa181038c52153141a850b4da
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "06c8cb482dd490ec2098284f9b765aa4",
+"flutter_bootstrap.js": "daf7045835cd951a42d77766309fcbfd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
